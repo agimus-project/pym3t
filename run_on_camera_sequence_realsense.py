35bd48919cd531a4b741fd5ee5eec6d6bbe58910
mfourmy <mederic.fourmy@gmail.com>
2023-01-04 20:03:31 +0100

    Bound most necessary methods for running the demo

--- run_on_camera_sequence_realsense.py
+++ run_on_camera_sequence_realsense.py
@@ -2,6 +2,7 @@ import numpy as np
 import pyicg
 
 body_name = 'banana'
+detector_name = 'toto.yaml'
 directory = 'toto/blabla'
 
 tracker = pyicg.Tracker('tracker')
@@ -22,6 +23,24 @@ depth_depth_renderer = pyicg.FocusedBasicDepthRenderer("depth_depth_renderer", r
 
 metafile_path = directory + body_name
 body = pyicg.Body(body_name, metafile_path)
-# renderer_geometry.AddBody(body)
-# color_depth_renderer_ptr.AddReferencedBody(body)
-# depth_depth_renderer_ptr.AddReferencedBody(body)
+renderer_geometry.AddBody(body)
+color_depth_renderer.AddReferencedBody(body)
+depth_depth_renderer.AddReferencedBody(body)
+
+detector_path = directory + detector_name
+detector = pyicg.StaticDetector("static_detector", detector_path, body)
+tracker.AddDetector(detector)
+
+region_model = pyicg.RegionModel(body_name + "_region_model", body, directory + body_name + "_region_model.bin")
+depth_model = pyicg.DepthModel(body_name + "_depth_model", body, directory + body_name + "_depth_model.bin")
+
+region_modality = pyicg.RegionModality(body_name + "_region_modality", body, color_camera, region_model)
+depth_modality = pyicg.DepthModality(body_name + "_depth_modality", body, depth_camera, depth_model)
+
+optimizer = pyicg.Optimizer(body_name+'_optimizer')
+optimizer.AddModality(region_modality)
+optimizer.AddModality(depth_modality)
+tracker.AddOptimizer(optimizer)
+
+# tracker.SetUp()
+# tracker.RunTrackerProcess(execute_detection=True, start_tracking=False)
--- src/pyicg.cpp
+++ src/pyicg.cpp
@@ -54,12 +54,18 @@ PYBIND11_MODULE(_pyicg_mod, m) {
                       "name"_a, "n_corr_iterations"_a=5, "n_update_iterations"_a=2, "synchronize_cameras"_a=true, 
                       "cycle_duration"_a=std::chrono::milliseconds{33}, "visualization_time"_a=0, "viewer_time"_a=1)
         .def("SetUp", &Tracker::SetUp)
+        .def("RunTrackerProcess", &Tracker::RunTrackerProcess)
         .def("AddViewer", &Tracker::AddViewer)
+        .def("AddDetector", &Tracker::AddDetector)
+        .def("AddOptimizer", &Tracker::AddOptimizer)
         ;
 
     // RendererGeometry
     py::class_<icg::RendererGeometry, std::shared_ptr<icg::RendererGeometry>>(m, "RendererGeometry")
         .def(py::init<const std::string &>(), "name"_a)
+        .def("AddBody", &RendererGeometry::AddBody)
+        .def("DeleteBody", &RendererGeometry::DeleteBody)
+        .def("ClearBodies", &RendererGeometry::ClearBodies)
         ;
 
 
@@ -70,21 +76,10 @@ PYBIND11_MODULE(_pyicg_mod, m) {
             using icg::Camera::Camera;
 
             bool SetUp() override {
-                PYBIND11_OVERRIDE_PURE(
-                    bool,
-                    Camera,
-                    SetUp
-                );
-            }
+                PYBIND11_OVERRIDE_PURE( bool, Camera, SetUp);}
 
             bool UpdateImage(bool synchronized) override {
-                PYBIND11_OVERRIDE_PURE(
-                    bool,
-                    Camera,
-                    UpdateImage,
-                    synchronized
-                );
-            }
+                PYBIND11_OVERRIDE_PURE( bool, Camera, UpdateImage, synchronized);}
     };
 
     // Camera -> not constructible, just to be able to bind RealSenseColorCamera and enable automatic downcasting
@@ -113,21 +108,10 @@ PYBIND11_MODULE(_pyicg_mod, m) {
             using icg::Viewer::Viewer;
 
             bool SetUp() override {
-                PYBIND11_OVERRIDE_PURE(
-                    bool,
-                    Viewer,
-                    SetUp
-                );
-            }
+                PYBIND11_OVERRIDE_PURE( bool, Viewer, SetUp);}
 
             bool UpdateViewer(int save_index) override {
-                PYBIND11_OVERRIDE_PURE(
-                    bool,
-                    Viewer,
-                    UpdateViewer,
-                    save_index
-                );
-            }
+                PYBIND11_OVERRIDE_PURE( bool, Viewer, UpdateViewer, save_index);}
     };
 
     // Viewer
@@ -150,10 +134,11 @@ PYBIND11_MODULE(_pyicg_mod, m) {
         //               "name"_a, "renderer_geometry_ptr"_a, "world2camera_pose"_a, "intrinsics"_a, "image_size"_a=200, "z_min"_a=0.01f, "z_max"_a=5.0f)       
         .def(py::init<const std::string &, const std::shared_ptr<RendererGeometry> &, const std::shared_ptr<Camera> &, int, float, float>(),
                       "name"_a, "renderer_geometry_ptr"_a, "camera_ptr"_a, "image_size"_a=200, "z_min"_a=0.01f, "z_max"_a=5.0f)
+        .def("AddReferencedBody", &FocusedBasicDepthRenderer::AddReferencedBody)
         ;
     
     // Body
-    py::class_<Body>(m, "Body")
+    py::class_<Body, std::shared_ptr<icg::Body>>(m, "Body")
         .def(py::init<const std::string &, const std::filesystem::path &>(), "name"_a, "geometry_path"_a)
         .def(py::init<const std::string &, const std::filesystem::path &, float, bool, bool, const Transform3fA &, uchar>(),
                       "name"_a, "geometry_path"_a, "geometry_unit_in_meter"_a, "geometry_counterclockwise"_a, "geometry_enable_culling"_a, "geometry2body_pose"_a, "silhouette_id"_a=0)
@@ -161,15 +146,34 @@ PYBIND11_MODULE(_pyicg_mod, m) {
         .def_property("world2body_pose", &Body::world2body_pose, &Body::set_world2body_pose)
         ;
 
+    ///
+    class PyDetector: public icg::Detector {
+        public:
+            // Inherit the base class constructor
+            using icg::Detector::Detector;
+
+            bool SetUp() override {
+                PYBIND11_OVERRIDE_PURE( bool, Detector, SetUp);}
+
+            bool DetectBody() override {
+                PYBIND11_OVERRIDE_PURE( bool, Detector, DetectBody);}
+    };
+
+    // Detector
+    py::class_<Detector, PyDetector, std::shared_ptr<icg::Detector>>(m, "Detector");
+
+
     // StaticDetector
-    py::class_<StaticDetector>(m, "StaticDetector")
+    py::class_<StaticDetector, Detector, std::shared_ptr<icg::StaticDetector>>(m, "StaticDetector")
         .def(py::init<const std::string &, const std::shared_ptr<Body> &, const Transform3fA &>(),
                       "name"_a, "body_ptr"_a, "body2world_pose"_a)
+        .def(py::init<const std::string &, const std::filesystem::path &, const std::shared_ptr<icg::Body> &>(),
+                      "name"_a, "metafile_path"_a, "body_ptr"_a)
         ;
  
 
     // RegionModel
-    py::class_<RegionModel>(m, "RegionModel")
+    py::class_<RegionModel, std::shared_ptr<icg::RegionModel>>(m, "RegionModel")
         .def(py::init<const std::string &, const std::shared_ptr<Body> &, const std::filesystem::path &, 
                       float, int, int, float, float, bool, int>(),
                       "name"_a, "body_ptr"_a, "model_path"_a, 
@@ -177,28 +181,70 @@ PYBIND11_MODULE(_pyicg_mod, m) {
         ;
 
     // DepthModel
-    py::class_<DepthModel>(m, "DepthModel")
+    py::class_<DepthModel, std::shared_ptr<icg::DepthModel>>(m, "DepthModel")
         .def(py::init<const std::string &, const std::shared_ptr<Body> &, const std::filesystem::path &, 
                       float, int, int, float, float, bool, int>(),
                       "name"_a, "body_ptr"_a, "model_path"_a, 
                       "sphere_radius"_a=0.8f, "n_divides"_a=4, "n_points"_a=200, "max_radius_depth_offset"_a=0.05f, "stride_depth_offset"_a=0.002f, "use_random_seed"_a=false, "image_size"_a=2000)
         ;
 
-    // RegionModality (because of RegionModel)
-    py::class_<RegionModality>(m, "RegionModality")
+
+
+    ///
+    class PyModality: public icg::Modality {
+        public:
+            // Inherit the base class constructor
+            using icg::Modality::Modality;
+
+            bool SetUp() override {
+                PYBIND11_OVERRIDE_PURE(bool, icg::Modality, SetUp);}
+            bool StartModality(int iteration, int corr_iteration) override {
+                PYBIND11_OVERRIDE_PURE(bool, icg::Modality, StartModality, iteration, corr_iteration);}
+            bool CalculateCorrespondences(int iteration, int corr_iteration) override {
+                PYBIND11_OVERRIDE_PURE(bool, icg::Modality, CalculateCorrespondences, iteration, corr_iteration);}
+            bool VisualizeCorrespondences(int save_idx) override {
+                PYBIND11_OVERRIDE_PURE(bool, icg::Modality, VisualizeCorrespondences, save_idx);}
+            bool CalculateGradientAndHessian(int iteration, int corr_iteration, int opt_iteration) override {
+                PYBIND11_OVERRIDE_PURE(bool, icg::Modality, CalculateGradientAndHessian, iteration, corr_iteration, opt_iteration);}
+            bool VisualizeOptimization(int save_idx) override {
+                PYBIND11_OVERRIDE_PURE(bool, icg::Modality, VisualizeOptimization, save_idx);}
+            bool CalculateResults(int iteration) override {
+                PYBIND11_OVERRIDE_PURE(bool, icg::Modality, CalculateResults, iteration);}
+            bool VisualizeResults(int save_idx) override {
+                PYBIND11_OVERRIDE_PURE(bool, icg::Modality, VisualizeResults, save_idx);}
+
+    };
+
+    py::class_<Modality, PyModality, std::shared_ptr<icg::Modality>>(m, "Modality");
+
+    // RegionModality
+    py::class_<RegionModality, Modality, std::shared_ptr<icg::RegionModality>>(m, "RegionModality")
         .def(py::init<const std::string &, const std::shared_ptr<Body> &, const std::shared_ptr<ColorCamera> &, const std::shared_ptr<RegionModel> &>(),
                       "name"_a, "body_ptr"_a, "color_camera_ptr"_a, "region_model_ptr"_a)
+        .def_property("visualize_pose_result", &RegionModality::visualize_pose_result, &RegionModality::set_visualize_pose_result)
+        .def_property("visualize_lines_correspondence", &RegionModality::visualize_lines_correspondence, &RegionModality::set_visualize_lines_correspondence)
+        .def_property("visualize_points_correspondence", &RegionModality::visualize_points_correspondence, &RegionModality::set_visualize_points_correspondence)
+        .def_property("visualize_points_depth_image_correspondence", &RegionModality::visualize_points_depth_image_correspondence, &RegionModality::set_visualize_points_depth_image_correspondence)
+        .def_property("visualize_points_depth_rendering_correspondence", &RegionModality::visualize_points_depth_rendering_correspondence, &RegionModality::set_visualize_points_depth_rendering_correspondence)
+        .def_property("visualize_points_result", &RegionModality::visualize_points_result, &RegionModality::set_visualize_points_result)
+        .def_property("visualize_points_histogram_image_result", &RegionModality::visualize_points_histogram_image_result, &RegionModality::set_visualize_points_histogram_image_result)
+        .def_property("visualize_points_histogram_image_optimization", &RegionModality::visualize_points_histogram_image_optimization, &RegionModality::set_visualize_points_histogram_image_optimization)
+        .def_property("visualize_points_optimization", &RegionModality::visualize_points_optimization, &RegionModality::set_visualize_points_optimization)
+        .def_property("visualize_gradient_optimization", &RegionModality::visualize_gradient_optimization, &RegionModality::set_visualize_gradient_optimization)
+        .def_property("visualize_hessian_optimization", &RegionModality::visualize_hessian_optimization, &RegionModality::set_visualize_hessian_optimization)
+        .def("MeasureOcclusions", &RegionModality::MeasureOcclusions)
         ;
 
 
-    // DepthModality (because of DepthModel)
-    py::class_<DepthModality>(m, "DepthModality")
+    // DepthModality
+    py::class_<DepthModality, Modality, std::shared_ptr<icg::DepthModality>>(m, "DepthModality")
         .def(py::init<const std::string &, const std::shared_ptr<Body> &, const std::shared_ptr<DepthCamera> &, const std::shared_ptr<DepthModel> &>(),
                       "name"_a, "body_ptr"_a, "depth_camera_ptr"_a, "depth_model_ptr"_a)
+        .def("MeasureOcclusions", &DepthModality::MeasureOcclusions)
         ;
 
     // Optimizer
-    py::class_<Optimizer>(m, "Optimizer")
+    py::class_<Optimizer, std::shared_ptr<icg::Optimizer>>(m, "Optimizer")
         .def(py::init<const std::string &, float, float>(),
                       "name"_a, "tikhonov_parameter_rotation"_a=1000.0f, "tikhonov_parameter_translation"_a=30000.0f)
         .def(py::init<const std::string &, const std::filesystem::path &>(),
@@ -207,6 +253,7 @@ PYBIND11_MODULE(_pyicg_mod, m) {
         .def_property("metafile_path", &Optimizer::metafile_path, &Optimizer::set_metafile_path)
         .def_property("tikhonov_parameter_rotation", &Optimizer::tikhonov_parameter_rotation, &Optimizer::set_tikhonov_parameter_rotation)
         .def_property("tikhonov_parameter_translation", &Optimizer::tikhonov_parameter_translation, &Optimizer::set_tikhonov_parameter_translation)
+        .def("AddModality", &Optimizer::AddModality)
         ;
 
 }   
