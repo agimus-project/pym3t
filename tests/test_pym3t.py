81948444d31eec75ad9f41c4a4c65cd235834fe0
mfourmy <mederic.fourmy@gmail.com>
2025-09-03 19:18:07 +0200

    remove pytest dep

--- tests/test_pym3t.py
+++ tests/test_pym3t.py
@@ -2,9 +2,7 @@
 import numpy as np
 import pym3t
 
-import pytest
-
-
+import unittest
 
 """
 Checking that type casting works properly.
@@ -13,39 +11,41 @@ Use dummy cameras class for now.
 Would be better to decouple the 2 tests in the future by defining 
 special checking fonctions in C++ and binding them.
 """
-def test_types_and_dummy_camera():
-    color_camera = pym3t.DummyColorCamera('cam_color')
-    depth_camera = pym3t.DummyDepthCamera('cam_color')
+class MyTestCase(unittest.TestCase):
 
-    # numpy <--> Transform3fA aka Eigen::Transform<float, 3, Eigen::Affine>
-    T1 = np.random.random((4,4)).astype(np.float64)
-    T2 = np.random.random((4,4)).astype(np.float32)
-    color_camera.world2camera_pose = T1
-    assert(color_camera.world2camera_pose.dtype == np.float32)
-    assert(color_camera.world2camera_pose.shape == (4,4))
-    assert(np.isclose(color_camera.world2camera_pose, T1).any())
-    color_camera.world2camera_pose = T2
-    assert(color_camera.world2camera_pose.dtype == np.float32)
-    assert(color_camera.world2camera_pose.shape == (4,4))
-    assert(np.isclose(color_camera.world2camera_pose, T2).any())
+    def test_types_and_dummy_camera(self):
+        color_camera = pym3t.DummyColorCamera('cam_color')
+        depth_camera = pym3t.DummyDepthCamera('cam_color')
 
-    # numpy <--> cv::Mat
-    color_size = (60,40,3)
-    depth_size = (80,50)
-    img_rgb = np.random.random(color_size).astype(np.uint8)
-    img_depth = np.random.random(depth_size).astype(np.uint16)
-    color_camera.image = img_rgb
-    depth_camera.image = img_depth
-    assert(color_camera.image.shape == color_size)
-    assert(depth_camera.image.shape == depth_size)
-    assert(color_camera.image.dtype == np.uint8)
-    assert(depth_camera.image.dtype == np.uint16)
+        # numpy <--> Transform3fA aka Eigen::Transform<float, 3, Eigen::Affine>
+        T1 = np.random.random((4,4)).astype(np.float64)
+        T2 = np.random.random((4,4)).astype(np.float32)
+        color_camera.world2camera_pose = T1
+        assert(color_camera.world2camera_pose.dtype == np.float32)
+        assert(color_camera.world2camera_pose.shape == (4,4))
+        assert(np.isclose(color_camera.world2camera_pose, T1).any())
+        color_camera.world2camera_pose = T2
+        assert(color_camera.world2camera_pose.dtype == np.float32)
+        assert(color_camera.world2camera_pose.shape == (4,4))
+        assert(np.isclose(color_camera.world2camera_pose, T2).any())
 
-    # Error raising when passing wrong types
-    with pytest.raises(ValueError):
-        color_camera.image = img_depth
-    with pytest.raises(ValueError):
-        depth_camera.image = img_rgb
+        # numpy <--> cv::Mat
+        color_size = (60,40,3)
+        depth_size = (80,50)
+        img_rgb = np.random.random(color_size).astype(np.uint8)
+        img_depth = np.random.random(depth_size).astype(np.uint16)
+        color_camera.image = img_rgb
+        depth_camera.image = img_depth
+        assert(color_camera.image.shape == color_size)
+        assert(depth_camera.image.shape == depth_size)
+        assert(color_camera.image.dtype == np.uint8)
+        assert(depth_camera.image.dtype == np.uint16)
+
+        # Error raising when passing wrong types
+        with self.assertRaises(ValueError):
+            color_camera.image = img_depth
+        with self.assertRaises(ValueError):
+            depth_camera.image = img_rgb
 
 
 
